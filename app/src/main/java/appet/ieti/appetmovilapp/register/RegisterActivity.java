a3a5c8e1f4cf66e4cf2d442c2a75c522ec0bbddd
DanielAlfonso17 <danielalfonso.29@hotmail.com>
2020-11-18 21:34:17 -0500

    Login y parte del register

---
+++ app/src/main/java/appet/ieti/appetmovilapp/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package appet.ieti.appetmovilapp.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import appet.ieti.appetmovilapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
